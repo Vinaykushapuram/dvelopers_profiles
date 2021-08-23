fb354f8e3f75b83bd487fdb63bf55d40219dec73
Vinay Kushapuram <reddyvinay172@gmail.com>
2021-08-23 19:25:51 +0530

    Add: Codeforces Rating Fetch

--- back_end/routes/helper.js
+++ back_end/routes/helper.js
@@ -3,6 +3,21 @@
     
 // }
 
+const fetchCodeforcesMaxRating = codeforces_id => {
+    return new Promise((resolve,reject)=>{
+        fetch(`https://codeforces.com/api/user.info?handles=${codeforces_id}`)
+            .then(res=>{
+                if(res.status==="OK"){
+                    resolve({rating:res.result[0].rating,maxRating:res.result[0].maxRating});
+                }
+                else{
+                    reject("error");
+                }
+            })
+            .catch(err=>err);
+    });
+}
+
 const updateRepos= github_id=>{
     return new Promise((resolve,reject)=>{
         fetch(`https://api.github.com/users/${github_id}/repos`)
